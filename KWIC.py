110fcd04518a632fa5735d41cc700add3aadb6d7
suhasramesh111 <suhas.ramesh.111@gmail.com>
2024-09-25 22:14:32 -0500

    Updated System architecture based on the feedback from prof. Chung

--- KWIC.py
+++ KWIC.py
@@ -1,192 +1,262 @@
 class master:
     """
-    The Master Control of the KWIC system. Manages the coordination of the entire process
-    including input reading, circular shifting, alphabetizing, and output.
+        This class is responsible for the overall orchestration of the KWIC system.
+        It manages the line storage, input handling, circular shift generation, 
+        alphabetization, and output.
+    
+        Attributes:
+        lineStorage : LineStorage - Object to store lines and shifts.
+        inputModule : InputModule - Object for reading lines.
+        circularShift : CircularShift - Object for generating circular shifts.
+        alphabetizer : Alphabetizer - Object for sorting shifts alphabetically.
+        output : Output - Object for printing results.
     """
 
     def __init__(self):
         """
-        Initializes the Master Control, setting up the LineStorage, InputModule,
-        CircularShift, Alphabetizer, and Output components.
+            Initializes the Master control class by creating instances of the components.
         """
 
         self.lineStorage = LineStorage()
-        self.inputModule = InputModule(self.lineStorage)
+        self.inputModule = InputModule()
         self.circularShift = CircularShift()
-        self.alphabetizer = Alphabetizer(self.circularShift, self.lineStorage)
-        self.output = Output(self.alphabetizer)
+        self.alphabetizer = Alphabetizer()
+        self.output = Output()
 
     def process_line(self, line):
         """
-        Processes a single line by reading, shifting, alphabetizing, and updating the KWIC index.
-
-        Parameters:
-        line (str): A line of text to be processed.
+            Processes a single line of input through the KWIC system.
+            
+            Parameters:
+            line : str - The input line to be processed.
+            
+            Returns: None
         """
 
-        self.inputModule.read(line)
-        self.circularShift.setup(self.lineStorage.getline(-1), self.lineStorage.word(-1))
-        self.alphabetizer.alpha()
+        self.inputModule.read(line, self.lineStorage)
+        self.circularShift.setup(self.lineStorage)
+        self.alphabetizer.alpha(self.lineStorage)
+        self.output.print_all_KWIC(self.lineStorage)
+        
 
 
 class InputModule:
     """
-    Handles the input and storage of lines into LineStorage.
+        This class handles reading the input line and storing it in the LineStorage.
     """
 
-    def __init__(self, lineStorage):
+    def read(self, line, lineStorage):
         """
-        Initializes the InputModule.
-
-        Parameters:
-        lineStorage (LineStorage): An instance of LineStorage where lines will be stored.
-        """
-
-        self.lineStorage = lineStorage
-
-    def read(self, line):
-        """
-        Reads a line and stores it into LineStorage.
-
-        Parameters:
-        line (str): A line of text to be read and stored.
+            Reads a line of input and stores it in the 'lines' array of LineStorage.
+            
+            Parameters:-
+            line : str - The input line to be read.
+            lineStorage : LineStorage - The storage object to hold the input line.
+            
+            Returns: None
         """
 
         self.line = line
-        self.lineStorage.setline(self.line)
+        lineStorage.setline(self.line, 'lines')
 
 
 class LineStorage:
     """
-    Stores lines of text and keeps track of sorted circular shifts.
+        This class is responsible for storing lines and shifts, 
+        as well as managing various arrays (lines, sorted shifts, temporary shifts).
     """
 
     def __init__(self):
         """
-        Initializes the LineStorage with empty lines and sorted shifts lists.
+            Initializes the arrays for storing lines, sorted shifts, and temporary shifts.
         """
 
         self.lines = []
         self.sorted_shifts = []
+        self.temp_shifts = []
 
-    def setline(self, line):
+    def setline(self, line, array_name):
         """
-        Adds a line to the stored lines.
-
-        Parameters:
-        line (str): A line of text to be added to the storage.
-        """
-
-        self.lines.append(line)
-
-    def getline(self, indx):
-        """
-        Retrieves a stored line based on the index.
-
-        Parameters:
-        indx (int): The index of the line to retrieve.
-
-        Returns:
-        str: The line of text at the specified index.
+            Adds a line to the specified array in LineStorage.
+            
+            Parameters:-
+            line : str - The line to be added.
+            array_name : str - Specifies which array ('lines', 'sorted_shifts', or 'temp_shifts') the line should be added to.
+            
+            Returns: None
         """
 
-        return self.lines[indx]
+        if array_name == 'lines':
+            array = self.lines            
+        elif array_name == 'sorted_shifts':
+            array = self.sorted_shifts            
+        else:
+            array = self.temp_shifts   
+        array.append(line)
 
-    def word(self, indx):
+    def getline(self, indx, array_name):
         """
-        Counts the number of words in a specific line.
-
-        Parameters:
-        indx (int): The index of the line to count words from.
-
-        Returns:
-        int: The number of words in the line.
+            Retrieves a line from the specified array.
+            
+            Parameters:-
+            indx : int - The index of the line to retrieve.
+            array_name : str - Specifies which array ('lines', 'sorted_shifts', or 'temp_shifts') to retrieve from.
+            
+            Returns:
+            list or str - The line from the specified array.
         """
+        
+        if array_name == 'lines':
+            array = self.lines            
+        elif array_name == 'sorted_shifts':
+            array = self.sorted_shifts            
+        else:
+            array = self.temp_shifts
+        return array[indx]
+    
+    def getArray(self, array_name):
+        """
+            Retrieves the entire array from LineStorage.
+            
+            Parameters:-
+            array_name : str - Specifies which array ('lines', 'sorted_shifts', or 'temp_shifts') to retrieve.
+            
+            Returns:
+            list - The specified array.
+        """
+        
+        if array_name == 'lines':
+            array = self.lines            
+        elif array_name == 'sorted_shifts':
+            array = self.sorted_shifts            
+        elif array_name == 'temp_shifts':
+            array = self.temp_shifts
+        else:
+            print("Invalid array name...")
+            return            
+        return array
+    
+    def update_array(self, new_array, array_name):
+        """
+            Updates the specified array with a new array.
+            
+            Parameters:-
+            new_array : list - The new array to replace the existing one.
+            array_name : str - Specifies which array ('lines', 'sorted_shifts', or 'temp_shifts') to update.
+            
+            Returns: None
+        """
+        
+        if array_name == 'lines':
+            self.lines = new_array
+        elif array_name == 'sorted_shifts':
+            self.sorted_shifts = new_array
+        elif array_name == 'temp_shifts':
+            self.temp_shifts = new_array
+        else:
+            print("Invalid array name...")
+            return
+    
 
-        return len(self.lines[indx].split())
+    def word(self, indx, array_name):
+        """
+            Returns the number of words in the line at the specified index.
+            
+            Parameters:-
+            indx : int - The index of the line.
+            array_name : str - Specifies which array ('lines', 'sorted_shifts', or 'temp_shifts') to count words from.
+            
+            Returns:
+            int - The number of words in the line.
+        """
+        
+        if array_name == 'lines':
+            array = self.lines            
+        elif array_name == 'sorted_shifts':
+            array = self.sorted_shifts            
+        else:
+            array = self.temp_shifts            
+        return len(array[indx].split())
 
 
 class CircularShift:
     """
-    Handles the creation of circular shifts of lines.
+        This class is responsible for generating circular shifts of the input lines.
     """
 
-    def __init__(self):
+    def setup(self, lineStorage):
         """
-        Initializes the CircularShift with an empty list of shifts.
+            Sets up the circular shift for the latest line stored in LineStorage.
+            
+            Parameters:-
+            lineStorage : LineStorage - The storage object containing the lines.
+            
+            Returns: None
         """
+        
+        line = lineStorage.getline(-1, 'lines')
 
-        self.shifts = []
-
-    def setup(self, line, n):
-        """
-        Sets up circular shifts for a given line.
-
-        Parameters:
-        line (str): The line of text to be circularly shifted.
-        n (int): The number of words in the line.
-        """
-
+        n = lineStorage.word(-1, 'lines')
+        shifts = lineStorage.temp_shifts
         words = line.split()
-        self.shifts.append(words)
-        self.shiftWords(n)
 
-    def shiftWords(self, n):
+        lineStorage.setline(words, 'temp_shifts')
+
+        self.shiftWords(n, shifts)
+        
+
+    def shiftWords(self, n, shifts):
         """
-        Generates all circular shifts for the current line.
-
-        Parameters:
-        n (int): The number of words in the line.
+            Generates circular shifts for a given line.
+            
+            Parameters:-
+            n : int - The number of words in the line.
+            shifts : list - The temporary shifts array where new shifts are stored.
+            
+            Returns: None
         """
 
         for i in range(n-1):
-            line_to_be_shifted = self.shifts[-1]
+            line_to_be_shifted = shifts[-1]
             shifted_line = [line_to_be_shifted[-1]] + line_to_be_shifted[:-1]
-            self.shifts.append(shifted_line)
 
-    def CSLine(self, indx):
+            shifts.append(shifted_line)
+
+    def CSLine(self, indx, lineStorage):
         """
-        Retrieves a circularly shifted line based on the index.
-
-        Parameters:
-        indx (int): The index of the circular shift to retrieve.
-
-        Returns:
-        list: The circularly shifted line as a list of words.
+            Retrieves a circular shift line by index.
+            
+            Parameters:-
+            indx : int - The index of the circular shift.
+            lineStorage : LineStorage - The storage object containing the shifts.
+            
+            Returns:
+            list - The circular shift at the specified index.
         """
 
-        return self.shifts[indx]
+        return lineStorage.getline(indx, 'temp_shifts')
 
 
 class Alphabetizer:
     """
-    Handles sorting circular shifts in lexicographic order and merging them into
-    the sorted list of shifts.
+        This class is responsible for sorting the circular shifts alphabetically.
     """
 
-    def __init__(self, circularShift, linestorage):
+    def alpha(self, lineStorage):
         """
-        Initializes the Alphabetizer.
-
-        Parameters:
-        circularShift (CircularShift): An instance of CircularShift to be alphabetized.
-        linestorage (LineStorage): An instance of LineStorage to store sorted shifts.
-        """
-
-        self.circularShift = circularShift
-        self.linestorage = linestorage
-
-    def alpha(self):
-        """
-        Sorts the circular shifts and merges them with the existing sorted shifts.
+            Sorts the circular shifts alphabetically and merges them with the existing sorted shifts.
+            
+            Parameters:-
+            lineStorage : LineStorage - The storage object containing the shifts.
+            
+            Returns: None
         """
 
-        shifts = self.circularShift.shifts
+        shifts = lineStorage.getArray('temp_shifts')
         shifts.sort()
-
-        sorted_shifts = self.linestorage.sorted_shifts
+        sorted_shifts = lineStorage.getArray('sorted_shifts')
         n = len(shifts)
-        m = len(self.linestorage.sorted_shifts)
+        m = len(lineStorage.sorted_shifts)
 
         if m == 0:
             sorted_shifts += shifts
@@ -198,63 +268,67 @@ class Alphabetizer:
         new_sorted_shift = []
 
         while i < n and j < m:
-            if shifts[i] < self.linestorage.sorted_shifts[j]:
+            if shifts[i] < sorted_shifts[j]:
                 new_sorted_shift.append(shifts[i])
                 i += 1
             else:
-                new_sorted_shift.append(self.linestorage.sorted_shifts[j])
+                new_sorted_shift.append(sorted_shifts[j])
                 j += 1
 
         new_sorted_shift.extend(shifts[i:])
-        new_sorted_shift.extend(self.linestorage.sorted_shifts[j:])
+        new_sorted_shift.extend(sorted_shifts[j:])
 
-        self.linestorage.sorted_shifts = new_sorted_shift
-        self.circularShift.shifts.clear()
+        lineStorage.update_array(new_sorted_shift, 'sorted_shifts')
+        shifts.clear()
 
-    def ith(self, indx):
+
+    def ith(self,lineStorage, indx):
         """
-        Retrieves the ith alphabetized circular shift.
-
-        Parameters:
-        indx (int): The index of the sorted circular shift to retrieve.
-
-        Returns:
-        list: The sorted circular shift at the specified index.
+            Retrieves the sorted circular shift at the specified index.
+            
+            Parameters:-
+            lineStorage : LineStorage - The storage object containing the sorted shifts.
+            indx : int - The index of the sorted shift.
+            
+            Returns:
+            list - The sorted circular shift at the specified index.
         """
-
-        return self.linestorage.sorted_shifts[indx]
+        
+        return lineStorage.getline(indx, 'sorted_shifts')
 
 
 class Output:
     """
-    Handles the output of KWIC lines.
+        This class is responsible for printing the KWIC results.
     """
 
-    def __init__(self, alphabetizer):
+    def print_all_KWIC(self, lineStorage):
         """
-        Initializes the Output class.
-
-        Parameters:
-        alphabetizer (Alphabetizer): An instance of Alphabetizer for retrieving sorted KWIC lines.
+            Prints all sorted circular shifts stored in LineStorage.
+            
+            Parameters:-
+            lineStorage : LineStorage - The storage object containing the sorted shifts.
+            
+            Returns: None
         """
-
-        self.alphabetizer = alphabetizer
-
-    def print_all_KWIC(self):
-        """
-        Prints all KWIC lines.
-        """
-
-        for line in self.alphabetizer.linestorage.sorted_shifts:
+       
+        sorted_shifts = lineStorage.sorted_shifts
+        for line in sorted_shifts:
             print(" ".join(line))
 
-    def print_KWIC(self, indx):
-        """
-        Prints the KWIC line at the specified index.
 
-        Parameters:
-        indx (int): The index of the KWIC line to print.
+    def print_KWIC(self, indx, lineStorage, alphabetizer):
+        """
+            Prints a specific KWIC result by index.
+           
+            Parameters:-
+            indx : int - The index of the sorted shift to print.
+            lineStorage : LineStorage - The storage object containing the sorted shifts.
+            alphabetizer : Alphabetizer - The alphabetizer to retrieve the sorted shifts.
+           
+            Returns:
+            None
         """
         
-        line = self.alphabetizer.ith(indx)
+        line = alphabetizer.ith(lineStorage, indx)
         print(" ".join(line))
